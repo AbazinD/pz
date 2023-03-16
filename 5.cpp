00f042959b858b6df38d7b2ddae43147244bb0de
prochorov <99527782+prochorov@users.noreply.github.com>
2023-03-16 23:59:06 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,49 +1,35 @@
 #include <iostream>
-#include <iomanip>
-#include <bitset>
-#include <vector>
 #include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
 
 using namespace std;
-
-const uint64_t MAX_NUM = 0xFFFFFFFFFFFFFFFFULL;
-const uint64_t BIT_MASK = 0x3ULL; // 0b11
 
 bool isPrime(uint64_t n)
 {
     if (n < 2)
         return false;
-    if (n == 2)
+    if (n == 2 || n == 3)
         return true;
-    if ((n & 1) == 0)
+    if (n % 2 == 0 || n % 3 == 0)
         return false;
-
-    uint64_t sqrt_n = sqrt(n);
-    for (uint64_t i = 3; i <= sqrt_n; i += 2) {
-        if (n % i == 0)
+    for (uint64_t i = 5; i * i <= n; i += 6)
+        if (n % i == 0 || n % (i + 2) == 0)
             return false;
-    }
-
     return true;
 }
 
 int main()
 {
     vector<uint64_t> primes;
-
-    for (uint64_t i = 3; i <= MAX_NUM; i += 2) {
-        if (i & BIT_MASK == BIT_MASK && isPrime(i)) {
-            primes.push_back(i);
-        }
+    for (uint64_t n = 0; n <= 0xFFFFFFFFFFFFFFFF; n++)
+    {
+        if ((n & (n - 1)) == 0 && isPrime(n))
+            primes.push_back(n);
     }
-
-    cout << "Found " << primes.size() << " primes with only two set bits." << endl;
-
-    if (!primes.empty()) {
-        sort(primes.begin(), primes.end());
-        cout << "Min prime: " << primes.front() << " (hex: " << hex << primes.front() << ")" << endl;
-        cout << "Max prime: " << primes.back() << " (hex: " << hex << primes.back() << ")" << endl;
-    }
-
+    sort(primes.begin(), primes.end());
+    cout << "Min prime: " << dec << primes.front() << " (hex: 0x" << hex << primes.front() << ")" << endl;
+    cout << "Max prime: " << dec << primes.back() << " (hex: 0x" << hex << primes.back() << ")" << endl;
     return 0;
 }
