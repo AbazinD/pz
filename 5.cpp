a93160515f000936057f144413205552eb5975c4
prochorov <99527782+prochorov@users.noreply.github.com>
2023-03-17 09:36:44 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,35 +1,45 @@
 #include <iostream>
 #include <cmath>
+#include <random>
 #include <vector>
 #include <algorithm>
-#include <iomanip>
-
 using namespace std;
 
-bool isPrime(uint64_t n)
+
+bool hasTwoSolo(unsigned long long n) {
+    int count = 0;
+
+    while (n)
+    {
+        if (n % 2 == 1) count++;
+        n /= 2;
+    }
+    if (n % 2 == 1) count++;
+    if (count == 2) return true;
+    return false;
+}
+
+bool isPrime(unsigned long long n)
 {
-    if (n < 2)
-        return false;
-    if (n == 2 || n == 3)
-        return true;
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-    for (uint64_t i = 5; i * i <= n; i += 6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
-    return true;
+ if (n<2)
+ return false;
+ if (n<4)
+ return true;
+ if ((n & 1) == 0)
+ return false;
+ for (unsigned long long i = 3; i <= (unsigned long long) sqrt(n); i+=2)
+ if (n % i == 0)
+ return false;
+ return true;
 }
 
 int main()
 {
-    vector<uint64_t> primes;
-    for (uint64_t n = 0; n <= 0xFFFFFFFFFFFFFFFF; n++)
-    {
-        if ((n & (n - 1)) == 0 && isPrime(n))
-            primes.push_back(n);
+    vector<uint64_t> v;
+    for (unsigned long long i = 1; i <= numeric_limits<unsigned long long>::max(); i+=2) {
+        if (hasTwoSolo(i) && isPrime(i)) {
+            cout << dec << i << " " << hex << i << endl;
+        }
     }
-    sort(primes.begin(), primes.end());
-    cout << "Min prime: " << dec << primes.front() << " (hex: 0x" << hex << primes.front() << ")" << endl;
-    cout << "Max prime: " << dec << primes.back() << " (hex: 0x" << hex << primes.back() << ")" << endl;
-    return 0;
+ return 0;
 }
